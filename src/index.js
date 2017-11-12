7f42e73ca817ab04d7e15704bec232e02fc72ecb
prashant-svmx <prashant.patil@servicemax.com>
2017-11-13 00:07:53 +0530

    Modified name of variable

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@
  * @param {string} locale
  * @return {string}
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+module.exports = function(num, locale) {
+    return num.toLocaleString(locale);
 };
